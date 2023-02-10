e4ae3c84298cdd04dfcff9fac1995b4d2a0f2ee8
Dylan CLAEYSENS <ony@macbook-pro-de-dylan.home>
2023-02-10 11:23:01 +0100

    first push

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const saucesRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/user');
+const path = require('path');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+mongoose.connect('mongodb+srv://Ony:muscle@cluster0.jnqu2js.mongodb.net/test',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// app.use(express.json());
+
+// app.use(bodyParser.json());
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
